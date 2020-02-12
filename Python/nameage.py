d521f9231c0d4d4a1a9dae5c161164bd085a412e
BRoe-Code <roettibn@mail.uc.edu>
2020-02-11 22:51:22 -0500

    New Python Scripts

--- Python/nameage.py
+++ Python/nameage.py
@@ -7,6 +7,16 @@ print('What is your name?')
 myName = input()
 print('Hello, ' + myName + '. That is a good name. How old are you?')
 myAge = input()
+if myAge < 13:
+    print("You're a minor?")
+ elif myAge == 13:
+    print("You're a teenager now... that's cool, I guess")
+ elif myAge > 13 and myAge < 30:
+    print("You're young and dumb")
+ elif myAge >= 30 and myAge < 65:
+    print("You're adulting.")
+ else:
+    print("... and you're not dead yet?")
 programAge = int(time.time() - start_time)
 print("%s? That's funny, I'm only a few seconds old." % myAge)
 print("I wish I was %s years old" % str(int(myAge) * 2))
