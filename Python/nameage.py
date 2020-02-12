80939f3c1f67977d61bec9686b7b9b269db5813a
BRoe-Code <roettibn@mail.uc.edu>
2020-02-11 20:36:22 -0500

    New Python Scripts

---
+++ Python/nameage.py
@@ -0,0 +1,15 @@
+﻿#First Python Script
+
+import time
+start_time = time.time()
+
+print('What is your name?')
+myName = input()
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+myAge = input()
+programAge = int(time.time() - start_time)
+print("%s? That's funny, I'm only a few seconds old." % myAge)
+print("I wish I was %s years old" % str(int(myAge) * 2))
+time.sleep(3)
+print("I'm tired. I go sleep sleep now.")
+print("%s? That's funny, I'm only %s seconds old." % (myAge, programAge))
