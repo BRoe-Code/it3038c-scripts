93494fbb01b3dc294aa2caf814e44d9190784324
BRoe-Code <roettibn@mail.uc.edu>
2020-04-19 18:56:20 -0400

    Updated Lab9

--- Python/Lab9.py
+++ Python/Lab9.py
@@ -4,17 +4,20 @@ import requests
 r = requests.get('http://localhost:3000/')
 data = r.json()
 
-print(data[0]['name'])
-print("is")
+print(data[0]['name'], end = ' ')
+print("is", end = ' ')
 print(data[0]['color'])
+print()
 print
 
-print(data[1]['name'])
-print("is")
+print(data[1]['name'], end = ' ')
+print("is", end = ' ')
 print(data[1]['color'])
+print()
 print
 
-print(data[2]['name'])
-print("is")
+print(data[2]['name'], end = ' ')
+print("is", end = ' ')
 print(data[2]['color'])
+print()
 print
